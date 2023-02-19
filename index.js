13a68cfba56682fd2ee609ef49b2605283c24046
Eoben <44886834+Ellioben@users.noreply.github.com>
2023-02-19 09:24:50 +0800

    Update index.js

--- index.js
+++ index.js
@@ -18,11 +18,13 @@ const readme = `\
 
 ⏳ Year progress ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} %
 
----
 
 ⏰ Updated on ${new Date().toUTCString()}
 
 ![Time bat](https://img.shields.io/badge/TimeBar-v1.0-red.svg)\
+
+---
+
 `
 
 console.log(readme)
