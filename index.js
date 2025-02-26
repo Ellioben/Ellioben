fd7ceb0b6dd52cf59fa58377686463e613862f1d
Eoben Xie <44886834+Ellioben@users.noreply.github.com>
2025-02-26 15:07:54 +0800

    Update index.js

--- index.js
+++ index.js
@@ -13,35 +13,18 @@ const progressBar =
 return `{ ${progressBar} }`
 }
 
-// 生成动态进度条图片URL
-function getProgressBar(percent) {
-  // 使用渐变参数（从红到紫的彩虹渐变）
-  const gradientParams = [
-    'type=gradient',          // 启用渐变模式
-    'gradient=ff0000,8f00ff', // 从红色到紫色
-    'steps=7'                 // 七色渐变
-  ].join('&');
-
-  return `https://progress-bar.dev/${percent}/?\
-&title=Year+Progress\
-&width=600\
-&suffix=%25\
-&${gradientParams}`;
-}
-
 const readme = `\
 ### Hi there 👋
 
 ⏳ Year progress ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} %
 
+---
 
 ⏰ Updated on ${new Date().toUTCString()}
 
 ![Time bat](https://img.shields.io/badge/TimeBar-v1.0-red.svg)\
 
 ---
-
-![Year Progress](${getProgressBar(progressOfThisYear * 100)})
 
 `
 
